7d0f2efe7b4d03ae89706cd0ca39170e4c235355
samim101 <samimreza2111@gmail.com>
2023-01-12 15:21:05 +0600

    nothing

--- C++/clp.c
+++ C++/clp.c
@@ -5,6 +5,20 @@ int main()
     freopen("/home/samim101/input.txt", "r", stdin);
     freopen("/home/samim101/output.txt", "w", stdout);
     #endif
-    
+    Name: Samim
+Surname: Reza
+Father’s Name:
+Mother’s Name:
+Date of Birth:
+Religion:
+Marital Status:
+Nationality:
+Contact Information:
+
+
+Phone Number:
+Email:
+
+
     
 }
